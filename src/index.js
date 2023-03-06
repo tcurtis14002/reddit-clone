07417d0b9547fd24cee5b44ad3c184bbf76c8621
Tyler <willdev0110@gmail.com>
2023-03-06 14:54:27 -0700

    create initial routing with react router

--- src/index.js
+++ src/index.js
@@ -2,16 +2,31 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import Header from "./components/Header/Header";
+import SideBar from "./components/SideBar/SideBar";
+import Feed from "./components/Feed/Feed";
+import Post from "./components/Post/Post.js";
 
-const router = createBrowserRouter(createRoutesFromElement(
-  <Route path='/' element={<Root />}>
-    <Route path='' element={<Header />} />
-    <Route path='' element={<SideBar />} />
-    <Route path='' element={<Feed />}>
-      <Route
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="header" element={<Header />} />
+      <Route path="sidebar" element={<SideBar />} />
+      <Route path="feed" element={<Feed />}>
+        <Route path="post" element={<Post />} />
+      </Route>
     </Route>
-  </Route>
-));
+  )
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
